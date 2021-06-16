6c9fdffdbcf7e4c14b54b023a8860abf177d2717
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2021-06-15 22:27:48 -0700

    Initial commit

---
+++ src/main/java/website/skylorbeck/sentimentality3/sentimentality3/Sentimentality3.java
@@ -0,0 +1,10 @@
+package website.skylorbeck.sentimentality3.sentimentality3;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Sentimentality3 implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/website/skylorbeck/sentimentality3/sentimentality3/client/Sentimentality3Client.java
@@ -0,0 +1,12 @@
+package website.skylorbeck.sentimentality3.sentimentality3.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class Sentimentality3Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
