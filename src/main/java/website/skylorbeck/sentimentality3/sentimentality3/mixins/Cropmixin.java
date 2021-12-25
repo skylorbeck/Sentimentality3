bacc66257d0567de32a8d65f67105602c281f127
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2021-12-25 08:43:41 -0800

    fixed overwrite

--- src/main/java/website/skylorbeck/sentimentality3/sentimentality3/mixins/Cropmixin.java
+++ src/main/java/website/skylorbeck/sentimentality3/sentimentality3/mixins/Cropmixin.java
@@ -38,7 +38,7 @@ public abstract class Cropmixin {
             cir.setReturnValue(!this.isMature(state));
         }
     }
-    @Inject(at = @At("HEAD"),cancellable = true,method = "randomTick")
+    @Inject(at = @At("HEAD"), method = "randomTick")
     private void sparkleTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
         if (Ref.cropSparkle) {
             ClientWorld clientWorld = MinecraftClient.getInstance().world;
